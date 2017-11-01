517343ac5dbb1bfc8ad55c586af8ae5c4fbeac71
atevington <andrew.tevington@gmail.com>
2017-11-01 10:37:01 -0400

    adjust symbol parse logic

--- email-parser/index.js
+++ email-parser/index.js
@@ -19,7 +19,7 @@ const marketVulture = (dom) => {
 
 		const message = dom(elem).next().text().toLowerCase()
 		const foundTextQuery = textQueries.filter(textQuery => message.indexOf(textQuery.text) !== -1)[0]
-		const symbol = (message.split(foundTextQuery ? foundTextQuery.text : "")[1] || "").split("at")[0].trim().toUpperCase()
+		const symbol = (message.split(foundTextQuery ? foundTextQuery.text : "")[1] || "").trim().split(" ")[0].toUpperCase()
 
 		if (foundTextQuery && symbol.length) {
 			moves.push({symbol, isBuy: foundTextQuery.isBuy})
