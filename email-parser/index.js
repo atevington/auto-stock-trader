e947ae5b71206d0a50db137ae0c5fac54729dbb6
atevington <andrew.tevington@gmail.com>
2017-12-01 13:04:26 -0500

    account for spoils for marketvulture

--- email-parser/index.js
+++ email-parser/index.js
@@ -1,25 +1,45 @@
+const symbolAfterQuery = (message, textQuery) => {
+	const part = (message.split(textQuery)[1] || "").trim()
+	return part.split(" ")[0].toUpperCase()
+}
+
+
+const symbolBeforeQuery = (message, textQuery) => {
+	const part = (message.split(textQuery)[0] || "").trim()
+	return part.split(" ")[part.split(" ").length - 1].toUpperCase()
+}
+
 const marketVulture = (dom) => {
 	const moves = []
+	
+	const textQueries = [
+		{
+			text: "we purchased the stock",
+			isBuy: true,
+			parse: symbolAfterQuery
+		},
+		{
+			text: "we doubled down on the stock",
+			isBuy: true,
+			parse: symbolAfterQuery
+		},
+		{
+			text: "we sold the stock",
+			isBuy: false,
+			parse: symbolAfterQuery
+		},
+		{
+			text: "has spoiled and we sold it",
+			isBuy: false,
+			parse: symbolBeforeQuery
+		}
+	]
 
 	dom("td:contains('Stock Picks')").each((index, elem) => {
-		const textQueries = [
-			{
-				text: "we purchased the stock",
-				isBuy: true
-			},
-			{
-				text: "we doubled down on the stock",
-				isBuy: true
-			},
-			{
-				text: "we sold the stock",
-				isBuy: false
-			}
-		]
-
 		const message = dom(elem).next().text().toLowerCase()
 		const foundTextQuery = textQueries.filter(textQuery => message.indexOf(textQuery.text) !== -1)[0]
-		const symbol = (message.split(foundTextQuery ? foundTextQuery.text : "")[1] || "").trim().split(" ")[0].toUpperCase()
+		console.log(foundTextQuery)
+		const symbol = foundTextQuery ? foundTextQuery.parse(message, foundTextQuery.text) : ""
 
 		if (foundTextQuery && symbol.length) {
 			moves.push({symbol, isBuy: foundTextQuery.isBuy})
